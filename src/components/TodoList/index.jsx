db1250acdd9da6b27adb35ce51c6c34d3c04100d
Ayça Kübra Soruklu <aycakubra1@gmail.com>
2025-06-01 15:18:05 +0200

    Updated TodoList component with necessary changes

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 
 const Index = () => {
-  const {data, isPending, isError, error}= useQuery({
+  const {data, isPending, isError, error, fetchStatus}= useQuery({
     queryFn: () =>
       fetch("https://jsonplaceholder.typicode.com/todos").then((response) =>
         response.json()
       ),
-      queryKey:"todos",
+      queryKey:"todos", 
+      
+      //todos sadece bu sorguyu tanımlamak için kullanılan key/string.
+      // React Query’nin cache ve veri yönetimi için zorunlu.
+      // API endpoint’ten veya başka bir yerden otomatik gelmez, sen veriyorsun.
+      
   });
   if(isPending){
     return <div>
@@ -15,6 +20,15 @@ const Index = () => {
   }
 
   if (isError) return <div>{error.message}</div>;
+
+  // fetchStatus === "fetching" Veri çekme işlemi devam ediyor (yeni veri çekiliyor)
+  // fetchStatus === "paused"  Veri çekme işlemi geçici durduruldu (örn. offline mod)
+  // fetchStatus === "idle" 	Veri çekme işlemi başlamadı, sorgu hazır durumda
+  
+  // fetchStatus, sorgunun veri çekme sürecinin durumunu belirtir.status ile karıştırma; 
+  // status genellikle loading, error, success gibi genel sonucu gösterir.
+  // fetchStatus ise veri çekme işleminin detayını (aktif mi, duraklatıldı mı) belirtir.
+  
 
   return (
     <div className="container mx-auto mt-10">
